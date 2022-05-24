ce4e39297d2aff0d2fa331c09f89b632f0c9a443
C:\Users\201910086\Desktop\likelion <201910086@sangmyung.kr>
2022-05-24 22:20:06 +0900

    코드 수정

--- 6주차과제/cafeproject/cafeproject/cafeproject/urls.py
+++ 6주차과제/cafeproject/cafeproject/cafeproject/urls.py
@@ -4,6 +4,8 @@ from cafeapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # url 바로 접속했을 때 home 함수 실행
     path('', views.home, name='home'),
+    # detail/로 갔을 때는 detail 함수 실행
     path('detail/', views.detail, name='detail'),
 ]
