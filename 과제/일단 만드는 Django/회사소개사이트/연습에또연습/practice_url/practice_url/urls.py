50ee4d386733e93461d7b56d731cd1b23e96d1f8
beeeeni <hyunbini2942@gmail.com>
2022-05-29 07:58:52 +0900

    6주차 과제 [박현빈]

---
+++ 과제/일단 만드는 Django/회사소개사이트/연습에또연습/practice_url/practice_url/urls.py
@@ -0,0 +1,29 @@
+"""practice_url URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from myapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.first),
+    path('second/', views.second),
+    path('thirds/', views.third),
+    # 'product/'라는 이름 하에 생기는 항목은 product.urls.py가 관리
+    path('products/', include('product.urls')),
+    # 위와 동일한 방식으로 board.urls.py가 관리
+    path('boards/', include('board.urls')),
+]
